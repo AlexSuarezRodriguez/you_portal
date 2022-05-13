ab6842e0099fcd893573f94e6f96f4425275196a
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-05-13 10:41:22 -0500

    ccomponent service

--- src/page/HomePage/index.jsx
+++ src/page/HomePage/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Presentation from '../../components/Presentation';
 import About from '../../components/About';
+import Serices from '../../components/Services';
 
 function Homepage() {
   return (
     <div style={{ heigth: '100vh' }}>
       <Presentation />
       <About />
+      <Serices />
     </div>
 
   );
