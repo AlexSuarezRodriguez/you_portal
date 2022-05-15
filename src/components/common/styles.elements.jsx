ef23bda1db39790eb5e98a1797a2293ee19ccc10
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-05-15 18:29:39 -0500

    se valida en un input el dato

--- src/components/common/styles.elements.jsx
+++ src/components/common/styles.elements.jsx
@@ -2,10 +2,15 @@
 export const Styles = {
   formContainer: {
     width: '100%',
-    heght: '98vh',
+    height: '98vh',
     display: 'flex',
-    flexFlow: 'row wrap',
+    flexFlow: 'row Wrap',
     justifyContent: 'center',
-    alignItemns: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  form: {
+    minHeight: '350px',
+    padding: '16px',
   },
 };
--- src/components/steps/FormComponent.jsx
+++ src/components/steps/FormComponent.jsx
@@ -1,13 +1,63 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable react/forbid-prop-types */
 /* eslint-disable react/prefer-stateless-function */
-import { Grid } from '@material-ui/core';
+import { Box, Grid, Paper, withStyles } from '@material-ui/core';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Styles } from '../common/styles.elements';
+import { renderInputText, renderText } from '../common/displayComponents';
 
-export default class FormComponent extends Component {
+class FormComponent extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    // eslint-disable-next-line react/no-unused-state
+    data: {
+      firstName: '',
+    },
+    // eslint-disable-next-line react/no-unused-state
+    errors: {},
+  };
+
   render() {
+    const { classes } = this.props;
+
+    const handleOnChange = ({ target }) => {
+      const { data, errors } = this.state;
+      function targetError() {
+        return (target.value.length <= 3
+          ? (errors[target.name] = `${target.name} debe contener mas de 3 caracteres`)
+          : (errors[target.name] = ''));
+      }
+      targetError();
+      data[target.name] = target.value;
+      this.setState({ data, errors });
+    };
     return (
-      <Grid container>
-        <Grid item xs={12} sm={7} />
+      <Grid container className={classes.formContainer}>
+        <Grid item xs={12} sm={7}>
+          <Box p={2} mb={2} component={Paper}>
+            {renderText({ label: 'Stepper component' })}
+          </Box>
+          <Box component={Paper}>
+            <form className={classes.form}>
+              {renderText({ label: 'Form component step 1' })}
+              <Box>
+                {renderInputText({
+                  label: ' First name',
+                  name: 'firstName',
+                  state: this.state,
+                  handleOnChange,
+                })}
+              </Box>
+            </form>
+          </Box>
+        </Grid>
       </Grid>
     );
   }
 }
+FormComponent.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(Styles)(FormComponent);
