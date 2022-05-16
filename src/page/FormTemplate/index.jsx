b588cc27a6cf322afc25b00ec8f41a220da5945d
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-05-15 23:08:24 -0500

    stepper first avance

--- src/page/FormTemplate/index.jsx
+++ src/page/FormTemplate/index.jsx
@@ -1,10 +1,70 @@
-import React from 'react';
-import FormComponent from '../../components/steps/formComponent';
+/* eslint-disable no-plusplus */
+/* eslint-disable no-unused-expressions */
+import { useState } from 'react';
+import Stepper from '../../components/Stepper';
+import StepperControl from '../../components/StepperControl';
+import { UseContextProvider } from '../../context/StepperContext';
 
-function FormTemplate() {
+import Account from '../../components/steps/Account';
+import Details from '../../components/steps/Details';
+import Payment from '../../components/steps/Payment';
+import Final from '../../components/steps/Final';
+
+function App() {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const steps = [
+    'Presentacion',
+    'Quien eres',
+    'Servicios',
+    'Contacto',
+    'Complete',
+  ];
+
+  // eslint-disable-next-line consistent-return
+  const displayStep = (step) => {
+    switch (step) {
+      case 1:
+        return <Account />;
+      case 2:
+        return <Details />;
+      case 3:
+        return <Payment />;
+      case 4:
+        return <Final />;
+      default:
+    }
+  };
+
+  const handleClick = (direction) => {
+    let newStep = currentStep;
+
+    direction === 'next' ? newStep++ : newStep--;
+    // check if steps are within bounds
+    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+  };
+
   return (
-    <FormComponent />
+    <div className="mx-auto rounded-2xl bg-white pb-2 shadow-xl md:w-1/2">
+      {/* Stepper */}
+      <div className="horizontal container mt-5 ">
+        <Stepper steps={steps} currentStep={currentStep} />
+
+        <div className="my-10 p-10 ">
+          <UseContextProvider>{displayStep(currentStep)}</UseContextProvider>
+        </div>
+      </div>
+
+      {/* navigation button */}
+      {currentStep !== steps.length && (
+        <StepperControl
+          handleClick={handleClick}
+          currentStep={currentStep}
+          steps={steps}
+        />
+      )}
+    </div>
   );
 }
 
-export default FormTemplate;
+export default App;
