cae3a1b2186e870133b738caff66059146953eec
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-05-20 16:49:17 -0500

    add array in information of component FormationAcademy

--- src/components/steps/FormationAcademic.jsx
+++ src/components/steps/FormationAcademic.jsx
@@ -1,103 +1,77 @@
-import { useState } from 'react';
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react';
 import { useStepperContext } from '../../context/StepperContext';
 
-export default function Account() {
-  const arrayFormation = [];
-  const [form, setForm] = useState([]);
-  // eslint-disable-next-line no-unused-vars
-  const { userData, setUserData } = useStepperContext();
+// eslint-disable-next-line react/prop-types
+function FormSchool({ onHandleSubmit }) {
+  const [school, setSchool] = useState({});
 
-  const handleChange = (formData) => {
-    setUserData({ ...userData, formation: [formData] });
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setSchool({ ...school, [name]: value });
   };
-  const handleChangeForm = (e) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
-    handleChange({ form });
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    onHandleSubmit(school);
   };
-  function handleSubmit() {
-    console.log('entro');
-    arrayFormation.push(form);
-    console.log('entro el dato es ', arrayFormation);
-    setForm([]);
-  }
-  console.log('esto es form', form);
-  console.log('esto es userData', userData);
-  console.log('esto es arrayFormation', arrayFormation);
+
   return (
-    <>
-      <div>
-        <form className="flex flex-col ">
-          <div className="mx-2 w-full flex-1">
-            <div className="mt-3 h-6 text-xs font-bold uppercase leading-8 text-gray-500">
-              institucion
-            </div>
-            <div className="my-2 flex rounded border border-gray-200 bg-white p-1">
-              <input
-                onChange={handleChangeForm}
-                name="institute"
-                placeholder="Nombre Completo"
-                className="w-full appearance-none p-1 px-2 text-gray-800 outline-none"
-              />
-            </div>
-          </div>
-          <div className="mx-2 w-full flex-1">
-            <div className="mt-3 h-6 text-xs font-bold uppercase leading-8 text-gray-500">
-              ¿ cuentame sobre  tus logros ?
-            </div>
-            <div className="my-2 flex rounded border border-gray-200 bg-white p-1">
-              <textarea
-                onChange={handleChangeForm}
-                name="achievements"
-                placeholder="como te describes"
-                type="text"
-                maxLength="250"
-                className="w-full h-24 appearance-none p-1 px-2 text-gray-800 outline-none flex  wrap"
-              />
-            </div>
-          </div>
-
-          <button type="button" onClick={handleSubmit}>
-            Guardar
-          </button>
-        </form>
+    <form onSubmit={handelSubmit} className="flex flex-col ">
+      <div className="mx-2 w-full flex-1">
+        <div className="mt-3 h-6 text-xs font-bold uppercase leading-8 text-gray-500">
+          institucion
+        </div>
+        <div className="my-2 flex rounded border border-gray-200 bg-white p-1">
+          <input
+            onChange={handleChange}
+            name="institute"
+            placeholder="Nombre Completo"
+            className="w-full appearance-none p-1 px-2 text-gray-800 outline-none"
+          />
+        </div>
       </div>
+      <div className="mx-2 w-full flex-1">
+        <div className="mt-3 h-6 text-xs font-bold uppercase leading-8 text-gray-500">
+          ¿ cuentame sobre  tus logros ?
+        </div>
+        <div className="my-2 flex rounded border border-gray-200 bg-white p-1">
+          <textarea
+            onChange={handleChange}
+            name="achievements"
+            placeholder="como te describes"
+            type="text"
+            maxLength="250"
+            className="w-full h-24 appearance-none p-1 px-2 text-gray-800 outline-none flex  wrap"
+          />
+        </div>
+      </div>
+      <button type="submit">agregar</button>
+    </form>
 
-      <form className="flex flex-col ">
-        <div className="mx-2 w-full flex-1">
-          <div className="mt-3 h-6 text-xs font-bold uppercase leading-8 text-gray-500">
-            institucion
-          </div>
-          <div className="my-2 flex rounded border border-gray-200 bg-white p-1">
-            <input
-              onChange={handleChangeForm}
-              name="institute1"
-              placeholder="Nombre Completo"
-              className="w-full appearance-none p-1 px-2 text-gray-800 outline-none"
-            />
-          </div>
-        </div>
-        <div className="mx-2 w-full flex-1">
-          <div className="mt-3 h-6 text-xs font-bold uppercase leading-8 text-gray-500">
-            ¿ cuentame sobre  tus logros ?
-          </div>
-          <div className="my-2 flex rounded border border-gray-200 bg-white p-1">
-            <textarea
-              onChange={handleChangeForm}
-              name="achievements1"
-              placeholder="como te describes"
-              type="text"
-              maxLength="250"
-              className="w-full h-24 appearance-none p-1 px-2 text-gray-800 outline-none flex  wrap"
-            />
-          </div>
-        </div>
-      </form>
-    </>
   );
 }
 
-// const { userData, setSTapper } = useContenxt()
-// const [form, setForm] = useState({})
-// const [schools, setChools] = useState([])
-// setStapper({ ...userData, formacion: [...schools, form]})
+export default function Account() {
+  const { userData, setUserData } = useStepperContext();
+  const [schools, setSchools] = useState([]);
+
+  const handleSubmit = (school) => {
+    const data = [...schools, school];
+    console.log(data);
+    setSchools(data);
+  };
+
+  useEffect(() => {
+    setUserData({ ...userData, formation: [schools] });
+  }, [schools]);
+  return (
+    <div>
+      {/* Cada vez que le den click al boton agregar metes elc omponente */}
+      <FormSchool onHandleSubmit={handleSubmit} userData={userData} />
+
+      {/* sera que requiere un boton */}
+      {/* aqui no va el boton */}
+    </div>
+  );
+}
--- src/page/FormTemplate/index.jsx
+++ src/page/FormTemplate/index.jsx
@@ -18,9 +18,9 @@ function App() {
 
   const steps = [
     'Presentacion',
-    'Quien eres',
-    'Servicios',
     'Formacion Academica',
+    'Servicios',
+    'Quien eres',
     'Contacto',
     'Complete',
     'final',
@@ -32,11 +32,11 @@ function App() {
       case 1:
         return <PresentationUser />;
       case 2:
-        return <About />;
+        return <FormationAcademic />;
       case 3:
         return <Service />;
       case 4:
-        return <FormationAcademic />;
+        return <About />;
       case 5:
         return <Contact />;
       case 6:
