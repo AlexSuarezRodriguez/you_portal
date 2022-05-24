f00665316154844f4c50a5fe9fd1d7683dbfbdb0
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-05-23 23:35:50 -0500

    first test end-to-end in register

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,17 +1,24 @@
 /* eslint-disable no-undef */
 /// <reference types="Cypress" />
-// const { faker } = require('@faker-js/faker');
+import { faker } from '@faker-js/faker';
 
 describe('you portal register', () => {
   it('should register ', () => {
+    const randomEmail = faker.internet.email();
+    const randomName = faker.name.findName();
+    const randomDescription = faker.lorem.paragraph(3);
     cy.visit('/');
     cy.get('[data-cy=\'register\']').click();
     cy.url().should('include', '/register');
-    cy.get('#email').clear().type('pruebaTest@');
+    cy.get('#email').clear().type('pruebaEmail');
     cy.get('#password').clear().type('123456');
     cy.get('[data-cy=\'button-register\']').click();
-    cy.get('#email').clear().type('pruebaTest@gmail.com');
+    cy.get('#email').clear().type(randomEmail);
     cy.get('#password').clear().type('123456asd');
     cy.get('[data-cy="button-register"]').click();
+    cy.url().should('include', '/formTemplate');
+    cy.get('#name').clear().type(randomName);
+    cy.get('#description').clear().type(randomDescription);
+    cy.get('#file').attachFile('index.jpg');
   });
 });
