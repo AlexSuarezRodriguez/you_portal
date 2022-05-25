294366558cd40a06dc0f72cff17fb154167b7b7f
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-05-25 18:51:18 -0500

    component Loading in homapage

--- src/page/HomePage/index.jsx
+++ src/page/HomePage/index.jsx
@@ -4,9 +4,9 @@ import About from '../../components/About';
 import Services from '../../components/Services';
 import Contact from '../../components/Contact';
 import Formation from '../../components/Formation';
-import { useAuth } from '../../context/authContext';
 import data from './db.json';
 import imageDefault from './images/87741454.jpg';
+import Loading from '../../components/Loading';
 
 function Homepage() {
   const {
@@ -27,41 +27,44 @@ function Homepage() {
     formation,
     experienceJob,
   } = data;
-  const { loading } = useAuth();
-
-  if (loading) return <h1>Cargando</h1>;
   return (
     <div style={{ heigth: '100vh' }}>
-      <Presentation
-        name={name}
-        description={description}
-        facebook={facebook}
-        numberContact={numberContact}
-        github={github}
-        image={imageDefault}
-      />
-      <About
-        introduction={introduction}
-        experience={experience}
-        proyects={proyects}
-        jobs={jobs}
-        image={imageDefault}
-      />
-      <Services
-        desing={desing}
-        front={front}
-        back={back}
-      />
-      <Formation
-        formation={formation}
-        experienceJob={experienceJob}
-      />
-      <Contact
-        email={email}
-        ubication={ubication}
-        facebook={facebook}
-        numberContact={numberContact}
-      />
+      {!data
+        ? <Loading />
+        : (
+          <>
+            <Presentation
+              name={name}
+              description={description}
+              facebook={facebook}
+              numberContact={numberContact}
+              github={github}
+              image={imageDefault}
+            />
+            <About
+              introduction={introduction}
+              experience={experience}
+              proyects={proyects}
+              jobs={jobs}
+              image={imageDefault}
+            />
+            <Services
+              desing={desing}
+              front={front}
+              back={back}
+            />
+            <Formation
+              formation={formation}
+              experienceJob={experienceJob}
+            />
+            <Contact
+              email={email}
+              ubication={ubication}
+              facebook={facebook}
+              numberContact={numberContact}
+            />
+          </>
+        )}
     </div>
   );
 }
